fbe272dab475c3e5599a8f280001d204b0596439
Dominika <nika.dziubinska@gmail.com>
2018-11-09 23:39:01 +0100

    changes

--- src/components/Form.js
+++ src/components/Form.js
@@ -16,20 +16,4 @@ const Form = (props) => {
 };
 
 
-
-
 export default Form;
-
-
-// import React, { Component } from 'react';
-//
-//
-// const Form = props => (
-//     <form className="form" onSubmit={props.getWeather}>
-//         <input type="text" name="city" placeholder="City..."/>
-//         <input type="text" name="country" placeholder="Country..."/>
-//         <button>Get Weather</button>
-//     </form>
-// );
-//
-// export default Form;
--- src/components/Titles.js
+++ src/components/Titles.js
@@ -8,17 +8,3 @@ const Titles = () => (
 );
 
 export default Titles;
-
-
-
-
-
-// import React, { Component } from 'react';
-// const Titles = () => (
-//     <div className="titles">
-//         <h1 className="title-container__title">Weather App</h1>
-//         <h3 className="title-container__subtitle">Find your city and check weather!</h3>
-//     </div>
-// );
-//
-// export default Titles;
